fc8accb613878fd3e019717da3f826681735d458
AlbertoSMC72 <223247@ids.upchiapas.edu.mx>
2024-02-07 09:35:23 -0600

    se agrego la authorization de github

--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express"
+import * as crypto from "crypto";
 
 const app = express();
 app.use(express.json());
@@ -11,7 +12,38 @@ app.listen(3000, () => {
     console.log('Server is running at 3000');
 });
 
-app.post("/github-event", (req: Request, res: Response) => {
+//funcion depara validar el secreto
+const webhooksSecret = "ultrakillGODGAME";
+
+const verify_signature = (req: Request) => {
+    try {
+        const signature = crypto
+            .createHmac("sha256", webhooksSecret)
+            .update(JSON.stringify(req.body))
+            .digest("hex");
+        let trusted = Buffer.from(`sha256=${signature}`, 'ascii');
+        let untrusted = Buffer.from(req.header("x-hub-signature-256") || "", "ascii");
+        return crypto.timingSafeEqual(trusted, untrusted);
+    }
+    catch (error) {
+        return false;
+    }
+};
+
+//middleware
+
+const verify_signature_middleware = (req: Request, res: Response, next: NextFunction) => {
+    if (!verify_signature(req)) {
+        res.status(401).json({
+            success: false,
+            message: "Sin autorizacion"
+        })
+        return;
+    }
+    next();
+}
+
+app.post("/github-event", verify_signature_middleware, (req: Request, res: Response) => {
     const { body } = req;
     const { action, sender, repository } = body;
     const event = req.headers['x-github-event'];
